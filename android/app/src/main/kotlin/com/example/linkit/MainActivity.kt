07a12250f9d1addf5c7e07878b4d57505aa76174
Latif_Kbr <abdelatif4614@gmail.com>
2022-10-07 20:20:09 +0100

    first creation

---
+++ android/app/src/main/kotlin/com/example/linkit/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.linkit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
+
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    DatabaseReference ref = FirebaseDatabase.instance.ref();
+    String dataStr = "";
+    getData() async {
+      final event = await ref.once(DatabaseEventType.value);
+      dataStr = event.snapshot.value.toString();
+    }
+
+    getData();
+    return Scaffold(
+      body: Center(
+        child: Text(dataStr),
+      ),
+    );
+  }
+}
